5fb59d232f486cf458095bdaa43362a8f08451e5
goldini102 <goldinistudios@gmail.com>
2025-01-12 14:03:12 -0500

    minor appearance update

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ for (var i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.textContent = this.textContent.replace("−", "+");
     } else {
       content.style.display = "block";
+      this.textContent = this.textContent.replace("+", "−");
     }
   });
 }
